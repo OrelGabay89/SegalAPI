9a7d4c7ae10d2957135853b7cd1c344078647e48
Orel <orelg@cymulate.com>
2024-06-04 15:06:04 +0300

    WIP

---
+++ ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using SegalAPI.Models;
+using System.Net;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IConfiguration _configuration;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IConfiguration configuration)
+    {
+        _next = next;
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var errorMessage = _configuration.GetValue<string>("ErrorDetails:DefaultErrorMessage") ?? "Internal Server Error.";
+
+        return context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = errorMessage
+        }.ToString());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
+using SegalAPI.Interfaces;
+using SegalAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();  // Register HttpClient
+builder.Services.AddSingleton<ITokenService, TokenService>();
 
 var app = builder.Build();
 
+var env = builder.Environment;
+var configuration = builder.Configuration;
+
+// Use Middleware to handle exceptions
+if (env.IsDevelopment() || configuration.GetValue<bool>("EnvironmentSettings:ShowDeveloperExceptions"))
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseMiddleware<ExceptionMiddleware>();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -15,11 +31,8 @@ app.UseSwaggerUI();
 app.UseExceptionHandler("/Home/Error");
 app.UseHsts();
 
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllers();
